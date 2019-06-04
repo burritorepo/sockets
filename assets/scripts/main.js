1aa467b4b40f9fb325e51a6578563d6b3c73005f
APPLE <apple@Mac-mini-de-APPLE.local>
2019-06-03 23:37:36 -0500

    it added chat

---
+++ assets/scripts/main.js
@@ -0,0 +1,56 @@
+function storage() {
+  function get(key = 'user') {
+    return sessionStorage.getItem(key);
+  }
+
+  function set(key = 'user', value = '') {
+    return sessionStorage.setItem(key, value)
+  }
+
+  function remove(key = 'user') {
+    return sessionStorage.removeItem(key)
+  }
+
+  function clear() {
+    return sessionStorage.clear()
+  }
+
+  return {
+    get,
+    set,
+    remove,
+    clear
+  }
+}
+
+function submitFormMessage(cb = () => {}) {
+  const form = document.querySelector('form');
+  const input = document.querySelector('input');
+  form.onsubmit = function(e) {
+    e.preventDefault();
+    cb(input.value)
+    input.value = '';
+  }
+}
+
+function htmlMessages(message) {
+  const container = document.querySelector('.js_messages')
+  const wrapper = document.createElement('div');
+
+  wrapper.classList.add('wrapp-message');
+  wrapper.innerHTML = `
+  <div class="client">${message.from} dice:</div>
+  <div class="message">${message.message}</div>
+  `;
+
+  container.appendChild(wrapper)
+}
+
+function socketWatch(channel, cb) {
+  socket.on(`${channel}`, cb)
+}
+
+
+function socketEmit(channel, value) {
+  socket.emit(`${channel}`, value)
+}
--- index.js
+++ index.js
@@ -1,26 +1,56 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-// app.use(express.static(__dirname + '/assets'));
+let usersConnected = [];
+
+app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/chat', function (req, res) {
   res.sendFile(__dirname + '/chat.html');
 });
 
-io.on('connection', function (socket) {
-    socket.on('disconnect', function () {
-    });
+app.get('/chat/:user', function (req, res) {
+  res.sendFile(__dirname + '/chat-user.html');
+});
 
-    socket.on('canalCliente', function (obj) {
-      io.emit('canalServer', obj);
-    })
+
+io.on('connection', function (socket) {
+  socket.on('disconnect', function () {
+  });
+
+  socket.on('chanel_messages', function (message) {
+    //  socket.broadcast.emit('message', obj);
+    io.emit('chanel_messages', message);
+  })
+
+  socket.on('chanel_list_users', function (user) {
+    usersConnected.push(user);
+
+    const userFiltered = usersConnected.reduce((acc, current) => {
+      const x = acc.find(item => item.user === current.user);
+      if (!x) {
+        return acc.concat([current]);
+      } else {
+        return acc;
+      }
+    }, []);
+
+    io.emit('chanel_list_users', userFiltered);
+  })
+
+
+  // socket.on('chat_detail', function(message) {
+  //   console.log('message detail', message)
+  // })
 });
 
 http.listen(3000, function () {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
+
